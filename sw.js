4bfc2fd3dfd6960e7f776984b6f1ffb67cd3e37b
Ibrahimshaa907 <iamibrahimshaa@gmail.com>
2025-09-15 11:53:04 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,21 @@ const CACHE_NAME = 'calculator-pwa-v1';
 const urlsToCache = [
   '/calculator/',
   '/calculator/index.html',
-  '/calculator/icons/icon-192x192.png',
-  '/calculator/logo.svg'
+  '/calculator/manifest.json'
 ];
 
+// Install event
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Cache opened');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
+// Fetch event
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
@@ -22,5 +24,21 @@ self.addEventListener('fetch', function(event) {
         return response || fetch(event.request);
       }
     )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if(cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
